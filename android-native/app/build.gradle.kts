a9aa53951342dcf2e0c8a0c0a0c0206972ef30d2
jayemm01 <johnmarius.peralta.cics@ust.edu.ph>
2026-05-20 18:14:13 +0800

    fix: remove invalid kotest-assertions-collections artifact

--- android-native/app/build.gradle.kts
+++ android-native/app/build.gradle.kts
@@ -135,7 +135,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-    testImplementation("io.kotest:kotest-assertions-collections:5.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
