866b558698566cfa816afd65ef6222705c91c1b9
jayemm01 <johnmarius.peralta.cics@ust.edu.ph>
2026-05-21 00:56:14 +0800

    fix: add explicit keyboard dismissal and scrollTo before validateButton click in instrumented tests

--- android-native/app/src/androidTest/java/com/implementsprint/mobile/MainActivityInstrumentedTest.kt
+++ android-native/app/src/androidTest/java/com/implementsprint/mobile/MainActivityInstrumentedTest.kt
@@ -1,9 +1,11 @@
 package com.implementsprint.mobile
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -27,7 +29,9 @@ class MainActivityInstrumentedTest {
         onView(withId(R.id.environmentInput)).perform(replaceText("uat"), closeSoftKeyboard())
         onView(withId(R.id.apiBaseUrlInput)).perform(replaceText(API_URL), closeSoftKeyboard())
 
-        onView(withId(R.id.validateButton)).perform(click())
+        // Explicitly dismiss keyboard and scroll to button before clicking
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.validateButton)).perform(scrollTo(), click())
 
         onView(withId(R.id.readinessStatusText)).check(matches(withText("Ready for deployment")))
     }
@@ -37,7 +41,9 @@ class MainActivityInstrumentedTest {
         onView(withId(R.id.environmentInput)).perform(replaceText("main"), closeSoftKeyboard())
         onView(withId(R.id.apiBaseUrlInput)).perform(replaceText("https://localhost/mock"), closeSoftKeyboard())
 
-        onView(withId(R.id.validateButton)).perform(click())
+        // Explicitly dismiss keyboard and scroll to button before clicking
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.validateButton)).perform(scrollTo(), click())
 
         onView(withId(R.id.readinessStatusText)).check(matches(withText(containsString("Not ready"))))
     }
