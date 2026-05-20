6da864b47ce46ea2ee131276b5dca1f45e70c976
jayemm01 <johnmarius.peralta.cics@ust.edu.ph>
2026-05-20 18:27:51 +0800

    fix: add missing PharmaCareApp, PrintService classes and fix MainActivity manifest reference

---
+++ android-native/app/src/main/java/com/pharmacare/pos/PharmaCareApp.kt
@@ -0,0 +1,13 @@
+package com.pharmacare.pos
+
+import android.app.Application
+
+/**
+ * Application class for PharmaCare POS.
+ * Registered in AndroidManifest.xml as android:name=".PharmaCareApp".
+ */
+class PharmaCareApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
